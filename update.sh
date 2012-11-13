81e0399cc6692e83e49bbaef1356123c1552d117
JiYou <jiyou09@gmail.com>
2012-11-14 00:33:54 +0800

    Update
    Signed-off-by: JiYou <jiyou09@gmail.com>

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 tsocks git config --global user.name "JiYou"
 tsocks git config --global user.email "jiyou09@gmail.com"
-tsocks git remote update origin git@github.com:JiYou/easyinstall.git
+git remote add origin git@github.com:JiYou/easyinstall.git
 tsocks git add .
 tsocks git commit -asm "Update"
 tsocks git push
